8fe871d85974dfe70b2cfefb51682e828cf99b76
Алексей Бубнов <avb_it@outlook.com>
2021-03-23 20:48:12 +0500

    создает модели для кабинета

--- cabinet/models.py
+++ cabinet/models.py
@@ -1,4 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
+class User(AbstractUser):
+    telegram_id = models.CharField(max_length=100)
+
+
+class Curencies(models.Model):
+    class Meta:
+        verbose_name = 'Валюта'
+        verbose_name_plural = 'Валюты'
+
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Purse(models.Model):
+    class Meta:
+        verbose_name = 'Кошелек'
+        verbose_name_plural = 'Кошельки'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Curencies, on_delete=models.CASCADE, verbose_name='Валюта')
+    ammount = models.FloatField(verbose_name='Количество')
+    coefficient = models.FloatField(verbose_name='Скидочный коэфициент')
+
+    def __str__(self):
+        return str(self.user)
