f462cd348582a9fde99bdfe1266de1b162d3195d
Алексей Бубнов <avb_it@outlook.com>
2021-10-28 17:56:35 +0500

    tinkoff notification

--- tinkoff_kassa/misc.py
+++ tinkoff_kassa/misc.py
@@ -1,6 +1,7 @@
 from hashlib import sha256
 from tinkoff_kassa.tinkoff_urls import *
 from django.conf import settings
+from loguru import logger
 
 
 def generate_token(request: dict):
@@ -13,6 +14,8 @@ def generate_token(request: dict):
     for key in keys:
         if key in unwanted_keys:
             continue
+        if isinstance(copy[key], bool):
+            copy[key] = str(copy[key]).lower()
         values_str += str(copy[key])
     return sha256(values_str.encode()).hexdigest()
 
