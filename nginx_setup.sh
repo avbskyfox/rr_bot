d1ba706a561f1161a3d95b82f635f72202c43197
Alex Bubnov <avb_it@outlook.com>
2022-02-20 15:26:30 +0500

    update ci

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 nginx -V
-yes | sudo cp -f etc/nginx/sites-avalible/rosreestr /etc/nginx/sites-avalible/rosreestr
-sudo ln -s /etc/nginx/sites-available/rosreestr /etc/nginx/sites-enabled/rosreestr
+yes | sudo cp -f etc/nginx/sites-avalible/rosreestr /etc/nginx/sites-availible/rosreestr
+sudo ln -f -s /etc/nginx/sites-available/rosreestr /etc/nginx/sites-enabled/rosreestr
 sudo /etc/init.d/nginx reload
