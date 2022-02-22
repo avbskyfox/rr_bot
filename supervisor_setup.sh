9779120858b32177c24e91e77c71819811faee4b
Alex Bubnov <avb_it@outlook.com>
2022-02-22 17:16:48 +0500

    принудительная перезагрузка сервисов при деплое

--- supervisor_setup.sh
+++ supervisor_setup.sh
@@ -3,4 +3,5 @@
 yes | sudo cp -f etc/supervisor/conf.d/asgi.conf /etc/supervisor/conf.d/asgi.conf
 yes | sudo cp -f etc/supervisor/conf.d/rrtelebot.conf /etc/supervisor/conf.d/rrtelebot.conf
 
-sudo supervisorctl update all
+sudo supervisorctl reread
+sudo supervisorctl restart all
