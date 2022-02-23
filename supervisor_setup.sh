3779112db19ab030154b5fb0c2f6fbb2eea34acd
Alex Bubnov <avb_it@outlook.com>
2022-02-23 23:10:53 +0500

    подготовка прод настроек

--- supervisor_setup.sh
+++ supervisor_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-yes | sudo cp -f etc/supervisor/conf.d/asgi.conf /etc/supervisor/conf.d/asgi.conf
-yes | sudo cp -f etc/supervisor/conf.d/rrtelebot.conf /etc/supervisor/conf.d/rrtelebot.conf
+yes | sudo cp -f etc/supervisor/conf.d/asgi_prod.conf /etc/supervisor/conf.d/asgi_prod.conf
+yes | sudo cp -f etc/supervisor/conf.d/rrtelebot_prod.conf /etc/supervisor/conf.d/rrtelebot_prod.conf
 
 sudo supervisorctl reread
 sudo supervisorctl restart all
