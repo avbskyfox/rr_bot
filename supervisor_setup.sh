938419d2e45c5efcf2ae67e7216516c67cb5130f
Alex Bubnov <avb_it@outlook.com>
2022-02-25 15:20:13 +0500

    фикс ошибки автосоздания кошелкька

--- supervisor_setup.sh
+++ supervisor_setup.sh
@@ -2,6 +2,7 @@
 
 yes | sudo cp -f etc/supervisor/conf.d/asgi.conf /etc/supervisor/conf.d/asgi.conf
 yes | sudo cp -f etc/supervisor/conf.d/rrtelebot.conf /etc/supervisor/conf.d/rrtelebot.conf
+yes | sudo cp -f etc/supervisor/conf.d/celery.conf /etc/supervisor/conf.d/celery.conf
 
 sudo supervisorctl reread
 sudo supervisorctl restart all
