57fec768533fa9abc6d1ff39f92905253eb48af1
Alex Bubnov <avb_it@outlook.com>
2022-02-25 16:11:12 +0500

    фикс ошибки автосоздания кошелкька

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 nginx -V
-yes | sudo cp -f etc/nginx/sites-avalible/rosreestr /etc/nginx/sites-availible/rosreestr
+yes | sudo cp -f etc/nginx/sites-avalible/rosreestr /etc/nginx/sites-available/rosreestr
 sudo ln -f -s /etc/nginx/sites-available/rosreestr /etc/nginx/sites-enabled/rosreestr
 sudo /etc/init.d/nginx reload
