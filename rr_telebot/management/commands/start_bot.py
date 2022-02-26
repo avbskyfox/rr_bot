734eaa9ec2709b9df3ef89b1da3d16128e8d31c8
Alex Bubnov <avb_it@outlook.com>
2022-02-27 00:19:44 +0500

    тип замка изменне со штатного redis.lock.Lock на самописный RedisLock

--- rr_telebot/management/commands/start_bot.py
+++ rr_telebot/management/commands/start_bot.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from rr_telebot.async_dispatcher import start
+import sentry_sdk
+
 
 
 class Command(BaseCommand):
@@ -10,4 +12,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         loglevel = options['loglevel']
+        sentry_sdk.init(
+            "https://glet_50f538f71aa1a73d0e00ef027887e7a1@gitlab.com/api/v4/error_tracking/collector/33259870",            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            # We recommend adjusting this value in production.
+            traces_sample_rate=1.0,
+        )
         start(loglevel)
