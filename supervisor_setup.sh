1063e89063f208dde15a4e84cfc12c4f5c26064c
Alex Bubnov <avb_it@outlook.com>
2022-02-26 14:31:25 +0500

    create celery_prod.conf

--- supervisor_setup.sh
+++ supervisor_setup.sh
@@ -3,6 +3,7 @@
 yes | sudo cp -f etc/supervisor/conf.d/asgi.conf /etc/supervisor/conf.d/asgi.conf
 yes | sudo cp -f etc/supervisor/conf.d/rrtelebot.conf /etc/supervisor/conf.d/rrtelebot.conf
 yes | sudo cp -f etc/supervisor/conf.d/celery.conf /etc/supervisor/conf.d/celery.conf
+yes | sudo cp -f etc/supervisor/conf.d/celery_prod.conf /etc/supervisor/conf.d/celery_prod.conf
 
 sudo supervisorctl reread
 sudo supervisorctl update all
